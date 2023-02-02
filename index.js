e2246b4746ba4ff807478b1eb57a43f0bc272484
Kelvin-owit <owitkelvin@gmail.com>
2023-02-02 20:40:12 +0300

    fifth commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var typed = new Typed(".input", {
   strings: ["Kelvin Owit", "A Frontend Developer", "A Web Developer"],
   typedSpeed: 70,
-  backSpeed: 55,
+  backSpeed: 65,
   loop: true,
 });
 
 // hamburger menu
 var hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function () {};
+hamburger.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("Active");
+};
+
+// copyright
+document.getElementById("copyright").innerHTML = new Date().getMonth();
